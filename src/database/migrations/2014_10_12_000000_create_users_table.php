acc8fc0b169e10d16e23c994ce9d4c1536c406e1
【daiki】 <daiki20010705@icloud.com>
2022-04-16 18:50:19 +0900

    [userテーブルのmigrate修正] ・不要なnameカラム削除 ・nullableの値を正しいものにした

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,16 +21,15 @@ class CreateUsersTable extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('first_name_ruby');
-            $table->string('last_name_ruby');
-            $table->string('nickname');
-            $table->string('sex');
-            $table->unsignedInteger('role_id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('first_name_ruby')->nullable();
+            $table->string('last_name_ruby')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('sex')->nullable();
+            $table->unsignedInteger('role_id')->nullable();
             $table->string('company')->nullable();
             $table->string('department')->nullable();
             $table->integer('working_period')->nullable();
