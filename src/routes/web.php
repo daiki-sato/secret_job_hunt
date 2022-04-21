2f14ffed67c889a61e4f5cdfbe0cb6cc0a44cdcb
【daiki】 <daiki20010705@icloud.com>
2022-04-22 03:18:13 +0900

    コンフリクト解消

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 // 未ログイン
 Route::middleware([])->group(function () {
-    Route::namespace('Auth')->group(function () {
+    Route::namespace ('Auth')->group(function () {
         // 登録
         Route::prefix('register')->group(function () {
-            Route::get('/',  'RegisterController@index')->name('register');
-            Route::get('/email',  'RegisterController@showRegistrationForm')->name('register.email');
+            Route::get('/', 'RegisterController@index')->name('register');
+            Route::get('/email', 'RegisterController@showRegistrationForm')->name('register.email');
             Route::post('/', 'RegisterController@register')->name('register.post');
             Route::get('/main_register/{token}', 'RegisterController@showForm');
             Route::post('/main_register', 'RegisterController@mainRegister')->name('register.main.registered');
@@ -23,17 +23,17 @@ Route::middleware([])->group(function () {
         Route::post('password/reset', 'ResetPasswordController@reset');
 
         // ログイン
-        Route::prefix('login')->group(function() {
-            Route::get('/',  'LoginController@showLoginForm')->name('login');
+        Route::prefix('login')->group(function () {
+            Route::get('/', 'LoginController@showLoginForm')->name('login');
             Route::post('/', 'LoginController@login')->name('login.post');
         });
 
         // ログアウト
         Route::get('/logout', 'LoginController@logout')->name('logout');
     });
-    
+
     // top
-    Route::get('/',         'TopController@index')->name('home');
+    Route::get('/', 'TopController@index')->name('home');
 });
 
 // ログイン済
@@ -42,21 +42,36 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['role:user'])->group(function () {
         // 検索
         Route::prefix('search')->group(function () {
-            Route::get('/',       'SearchController@index')->name('search');
+            Route::get('/', 'SearchController@index')->name('search');
         });
 
         // 予約一覧
         Route::prefix('reservation-list')->group(function () {
-            Route::get('/', 	    'ReservationListController@index')->name('reservation-list');
+            Route::get('/', 'ReservationListController@index')->name('reservation-list');
         });
-        
+
         Route::prefix('message')->group(function () {
-            Route::get('/', 	    'MessageController@index')->name('message');
-            Route::post('/', 	    'MessageController@store')->name('message.store');
+            Route::get('/', 'MessageController@index')->name('message');
+            Route::post('/', 'MessageController@store')->name('message.store');
         });
-        
+
         Route::prefix('my-page')->group(function () {
-            Route::get('/', 	    'MyPageController@index')->name('my-page');
+            Route::get('/', 'MyPageController@index')->name('my-page');
         });
+
+        //通話機能
+        Route::prefix('call')->group(function () {
+            Route::get('/', 'CallController@index')->name('call');
+        });
+
+        // チャット
+        Route::get('chat', 'ChatController@index');
+        Route::get('ajax/chat', 'Ajax\ChatController@index'); // メッセージ一覧を取得
+        Route::post('ajax/chat', 'Ajax\ChatController@create'); // チャット登録
+
+        // ビデオチャット
+        Route::get('video_chat', 'VideoChatController@index'); // チャットページ
+        Route::post('auth/video_chat', 'VideoChatController@auth'); // 認証ページ
+
     });
 });
