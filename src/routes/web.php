b7ca74d7c7b16058475daad94103c36c0056fdf5
【daiki】 <daiki20010705@icloud.com>
2022-04-23 21:44:26 +0900

    空白削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 // 未ログイン
 Route::middleware([])->group(function () {
-    Route::namespace('Auth')->group(function () {
+    Route::namespace ('Auth')->group(function () {
         // 登録
         Route::prefix('register')->group(function () {
-            Route::get('/',  'RegisterController@index')->name('register');
-            Route::get('/email',  'RegisterController@showRegistrationForm')->name('register.email');
+            Route::get('/', 'RegisterController@index')->name('register');
+            Route::get('/email', 'RegisterController@showRegistrationForm')->name('register.email');
             Route::post('/', 'RegisterController@register')->name('register.post');
             Route::get('/main_register/{token}', 'RegisterController@showForm');
             Route::post('/main_register', 'RegisterController@mainRegister')->name('register.main.registered');
@@ -23,18 +23,18 @@ Route::middleware([])->group(function () {
         Route::post('password/reset', 'ResetPasswordController@reset');
 
         // ログイン
-        Route::prefix('login')->group(function() {
-            Route::get('/',  'LoginController@index')->name('login.top');
-            Route::get('/email',  'LoginController@showLoginForm')->name('login.email');
+        Route::prefix('login')->group(function () {
+            Route::get('/', 'LoginController@index')->name('login.top');
+            Route::get('/email', 'LoginController@showLoginForm')->name('login.email');
             Route::post('/email', 'LoginController@login')->name('login.post');
         });
 
         // ログアウト
         Route::get('/logout', 'LoginController@logout')->name('logout');
     });
-    
+
     // top
-    Route::get('/',         'TopController@index')->name('home');
+    Route::get('/', 'TopController@index')->name('home');
 });
 
 // ログイン済
@@ -43,21 +43,21 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['role:user'])->group(function () {
         // 検索
         Route::prefix('search')->group(function () {
-            Route::get('/',       'SearchController@index')->name('search');
+            Route::get('/', 'SearchController@index')->name('search');
         });
 
         // 予約一覧
         Route::prefix('reservation-list')->group(function () {
-            Route::get('/', 	    'ReservationListController@index')->name('reservation-list');
+            Route::get('/', 'ReservationListController@index')->name('reservation-list');
         });
-        
+
         Route::prefix('message')->group(function () {
-            Route::get('/', 	    'MessageController@index')->name('message');
-            Route::post('/', 	    'MessageController@store')->name('message.store');
+            Route::get('/', 'MessageController@index')->name('message');
+            Route::post('/', 'MessageController@store')->name('message.store');
         });
-        
+
         Route::prefix('my-page')->group(function () {
-            Route::get('/', 	    'MyPageController@index')->name('my-page');
+            Route::get('/', 'MyPageController@index')->name('my-page');
         });
     });
 });
