1b6c557fc013d9950bfe3d95daaf5fad4e2cc103
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-04 03:02:13 +0900

    見た目ほぼ完成

--- src/app/Http/Controllers/EvaluationController.php
+++ src/app/Http/Controllers/EvaluationController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Console\Presets\React;
 use Illuminate\Http\Request;
 
 class EvaluationController extends Controller
 {
     public function index (){
+        return view('evaluation.index');
+    }
+    public function add (Request $request){
         return view('evaluation.index');
     }
 }
--- src/resources/views/evaluation/index.blade.php
+++ src/resources/views/evaluation/index.blade.php
@@ -5,6 +5,8 @@
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <link rel="stylesheet" href="{{ asset('css/evaluation/index.css') }}">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     </head>
 
     <!-- Button trigger modal -->
@@ -17,39 +19,36 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body">
-                    <h3>お疲れ様でした、面談はどうでしたか？</h2>
-                        <p>＊評価をもって面談完了となります。</p>
-                        <form action="" method="POST">
-                            @csrf
-                            <div class="btn">
-                                <input type="button" value="Good">
-                                <input type="button" value="Bad">
-                            </div>
-                            <div class="text">
-                                <p>ひとことでよいので理由を教えてください。</p>
-                                <input type="text" value="〇〇が良かった,悪かった,困ったなど...">
-                            </div>
-                            <div class="modal-footer">
-                                <input type="submit" value="フィードバックを送信する" class="btn btn-primary">
-                        </form>
+                    <h4>お疲れ様でした面談はどうでしたか？</h4>
+                    <p class="text-black-50">＊評価をもって面談完了となります。</p>
+                    <form action="{{ route('evaluation_add') }}" method="POST">
+                        @csrf
+                        <div class="btn">
+                            <input type="button" class="good btn btn-outline-secondary mr-4 " value="Good" id="1" name="good">
+                            <input type="button" class="bad btn btn-outline-secondary" value="Bad" id="2" name="bad">
+                        </div>
+                        <div class="text">
+                            <p class="mt-2 mb-0">ひとことでよいので理由を教えてください。</p>
+                            <input type="text" class="form-control col-xs-4" placeholder="〇〇が良かった,悪かった,困ったなど...">
+                        </div>
+                            <input type="submit" class="btn btn-primary mt-4" value="フィードバックを送信する">
+                    </form>
                 </div>
             </div>
-
         </div>
     </div>
-    </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-    <script src="{{ asset('js/styleindex.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-@endsection
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+        <script src="{{ asset('js/styleindex.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+    @endsection
