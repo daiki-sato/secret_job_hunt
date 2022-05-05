e3389253f59b61b792e530819758457488046702
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-05 12:31:57 +0900

    <head>タグを移植した

--- src/resources/views/evaluation/index.blade.php
+++ src/resources/views/evaluation/index.blade.php
@@ -2,12 +2,6 @@
 
 @section('content')
 
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/evaluation/index.css') }}">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    </head>
 
     <!-- Button trigger modal -->
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
