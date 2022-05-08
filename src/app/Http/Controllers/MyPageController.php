e0c6b66d781a23691e9135224a0218039025f6f9
【daiki】 <daiki20010705@icloud.com>
2022-05-08 14:26:07 +0900

    インデント整えた

--- src/app/Http/Controllers/MyPageController.php
+++ src/app/Http/Controllers/MyPageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,23 +10,22 @@ class MyPageController extends Controller
 {
     public function index()
     {
-
         $id = Auth::id();
         $user = User::find($id);
-        return view('mypage.index',compact('user'));
+        return view('mypage.index', compact('user'));
     }
 
     public function edit(Request $request)
     {
         $id = Auth::id();
         $user = User::find($id);
-        return view('mypage.user_edit',compact('user'));
+        return view('mypage.user_edit', compact('user'));
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         $user = User::find($id);
-        
+
         $user->first_name = $request->input('first_name');
         $user->last_name = $request->input('last_name');
         $user->first_name_ruby = $request->input('first_name_ruby');
