9afa2f0e10235e6b088d3c19e7d83b89ef51f116
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-09 02:07:49 +0900

    絞り込み後画面仮

--- src/resources/views/review/index.blade.php
+++ src/resources/views/review/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="d-flex  flex-column col-6 mx-auto">
-        <a href="{{ route('review-show', ['id' => 9]) }}" class="btn btn-primary mb-1 ">藤森日奈　女性　3年目</a>
-        <a href="{{ route('review-show', ['id' => 2]) }}" class="btn btn-primary">徳川家康　男性　1年目</a>
+        <a href="{{ route('review-show', ['id' => 2]) }}" class="btn btn-primary">user_id = 2</a>
+        <a href="{{ route('review-show', ['id' => 9]) }}" class="btn btn-primary mb-1 ">user_id = 9</a>
     </div>
 @endsection
