b8124623a3a6d5f6937c829b585c6076cfc3fb12
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-08 17:46:17 +0900

    管理者画面見た目作成, タブ切り替えが効かず詰まっています

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,7 +1,47 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="admin-index">
-      <h2 class="sample">adminです。（これ作業するとき消してね、仮で置いてるだけです。）</h2>
+    <div class="d-flex flex-row admin-index">
+      <div class="p-5 menu">
+        <input id="payment" type="radio" name="menu_item" checked>
+        <label class="menu_item h2 py-4 d-block" for="payment"><i class="pr-2 fa-brands fa-paypal"></i>支払い状況</label>
+        <input id="point-exchange" type="radio" name="menu_item">
+        <label class="menu_item h2 py-4 d-block" for="point-exchange"><i class="pr-2 fa-solid fa-money-bill-1"></i>換金状況</label>
+        <input id="contact" type="radio" name="menu_item">
+        <label class="menu_item h2 py-4 d-block" for="contact"><i class="pr-2 fa-solid fa-comment-dots"></i>お問合せ</label>
+      </div>
+      <div class="menu_content" id="payment_content">
+        <p>test</p>
+      </div>
+      <div class="menu_content" id="point-exchange_content">
+        <div class="tab_content_description">
+          <p class="c-txtsp">プログラミングの内容がここに入ります</p>
+        </div>
+      </div>
+      <div class="menu_content" id="contact_content">
+        <div class="p-5 admin-wrapper">
+          <div class="p-5 m-5 admin-inner">
+            <h2 class="pt-2 pb-5">お問合せ</h2>
+            <table class="w-100 profile_table">
+              <tbody class="text-center">
+                  <tr class="p-3 mx-3 my-5 chart-top">
+                      <td class="px-4 py-3">お問合せ日時</td>
+                      <td class="px-4 py-3">名前</td>
+                      <td class="px-4 py-3">項目</td>
+                      <td class="px-4 py-3">メッセージ内容</td>
+                      <td class="px-4 py-3">対応状況</td>
+                  </tr>
+                  <tr class="p-3 mx-3 my-5 chart-top">
+                      <td class="px-4 py-3">お問合せ日時</td>
+                      <td class="px-4 py-3">名前</td>
+                      <td class="px-4 py-3">項目</td>
+                      <td class="px-4 py-3">メッセージ内容</td>
+                      <td class="px-4 py-3">対応状況</td>
+                  </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
 @endsection
