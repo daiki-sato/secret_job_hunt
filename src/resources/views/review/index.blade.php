f0d978d370a5fe141eeb6f2c176a2cd184d341da
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-09 01:16:02 +0900

    絞り込み後画面修正

--- src/resources/views/review/index.blade.php
+++ src/resources/views/review/index.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-flex  flex-column col-6 mx-auto">
-<a href="{{route('review-show')}}" class="btn btn-primary mb-1 ">藤森日奈　女性　3年目</a>
-<a href="" class="btn btn-primary">徳川家康　男性　1年目</a>
-</div>
-
+    <div class="d-flex  flex-column col-6 mx-auto">
+        <a href="{{ route('review-show', ['id' => 9]) }}" class="btn btn-primary mb-1 ">藤森日奈　女性　3年目</a>
+        <a href="{{ route('review-show', ['id' => 2]) }}" class="btn btn-primary">徳川家康　男性　1年目</a>
+    </div>
 @endsection
