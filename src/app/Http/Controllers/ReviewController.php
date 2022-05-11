af311c6b0b476a381859335f3c7204ba07db964b
【daiki】 <daiki20010705@icloud.com>
2022-05-11 17:01:09 +0900

    mrpのコード修正

--- src/app/Http/Controllers/ReviewController.php
+++ src/app/Http/Controllers/ReviewController.php
@@ -2,63 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Call;
 use App\Models\User;
-use App\Models\Thread;
-
+use Illuminate\Support\Facades\DB;
 
 class ReviewController extends Controller
 {
     public function index()
     {
         $solvers = User::where('role_id', 3)->get();
-        return view('review.index',['solvers'=>$solvers]);
+        return view('review.index', ['solvers' => $solvers]);
     }
     public function show($solver_id)
     {
-
         $solver = User::where('id', $solver_id)->first();
 
-        $users_id = Call::where('solver_id', $solver_id)
+        $good_users = DB::table('calls')
+            ->where('solver_id', $solver_id)
             ->where('evaluation', true)
-            ->get('user_id');
-
-        $good_users_id = [];
-        foreach ($users_id as $good_user) {
-            array_push($good_users_id, $good_user->user_id);
-        };
-
-        $good_users = [];
-        foreach ($good_users_id as $good_user_id) {
-            array_push($good_users, User::where('id', $good_user_id)->first());
-        };
-
-        $bad_users_id = Call::where('solver_id', $solver_id)
-            ->where('evaluation', false)
-            ->pluck('user_id');
-
-        $bad_users = [];
-        foreach ($bad_users_id as $bad_user_id) {
-            array_push($bad_users, User::where('id', $bad_user_id)->first());
-        };
-
-
-
-        $good_reviews = Call::where('solver_id', $solver_id)
-            ->where('evaluation', true)
-            ->get();
-        $good_points = Call::where('solver_id', $solver_id)
-            ->where('evaluation', true)
-            ->count();
-
-        $bad_reviews = Call::where('solver_id', $solver_id)
-            ->where('evaluation', false)
+            ->join('users', 'users.id', '=', 'calls.user_id')
+            ->select('users.nickname', 'calls.evaluation_comment', 'calls.call_end_time', 'calls.evaluation')
             ->get();
 
-        $bad_points = Call::where('solver_id', $solver_id)
+        $bad_users = DB::table('calls')
+            ->where('solver_id', $solver_id)
             ->where('evaluation', false)
-            ->count();
-        return view('review.show', ['solver' => $solver, 'good_reviews' => $good_reviews, 'bad_reviews' => $bad_reviews, 'good_points' => $good_points, 'bad_points' => $bad_points, 'good_users' => $good_users,'bad_users' => $bad_users]);
+            ->join('users', 'users.id', '=', 'calls.user_id')
+            ->select('users.nickname', 'calls.evaluation_comment', 'calls.call_end_time', 'calls.evaluation')
+            ->get();
+
+        return view('review.show', ['solver' => $solver, 'good_users' => $good_users, 'bad_users' => $bad_users]);
     }
 }
--- src/resources/views/review/show.blade.php
+++ src/resources/views/review/show.blade.php
@@ -16,55 +16,49 @@
             <div class="content d-flex ">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                      <a class="nav-link active text-danger bg-white mr-5" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
-                        Good
-                        <p class="border-dark border-bottom text-dark">{{ $good_points }}</p>
-                      </a>
+                        <a class="nav-link active text-danger bg-white mr-5" id="pills-home-tab" data-toggle="pill"
+                            href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
+                            Good
+                            <p class="border-dark border-bottom text-dark">{{ $good_users->count('evaluation') }}</p>
+                        </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link text-primary bg-white" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
-                        Bad
-                        <p class="border-dark border-bottom text-dark">{{ $bad_points }}</p>
-                      </a>
+                        <a class="nav-link text-primary bg-white" id="pills-profile-tab" data-toggle="pill"
+                            href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
+                            Bad
+                            <p class="border-dark border-bottom text-dark">{{ $bad_users->count('evaluation') }}</p>
+                        </a>
                     </li>
-                  </ul>
+                </ul>
             </div>
             <div class="tab-content w-auto " id="pills-tabContent ">
                 <div class="tab-pane fade show active " id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                     @foreach ($good_users as $good_user)
-                        @foreach ($good_reviews as $good_review)
-                            @if ($loop->parent->first)
-                                <div class="reviewer  d-flex  justify-content-left mt-3">
-                                    <div class="img">
-                                        <img src="{{ asset('img/review/reviewer.png') }}" alt="プロフィール写真">
-                                    </div>
-                                    <div class="comment text-md-left">
-                                        <p class="name mb-1">{{ $good_user->nickname }}</p>
-                                        <p class="review text-wrap">{{ $good_review->evaluation_comment }}</p>
-                                        <p>{{ $good_review->call_end_time }}</p>
-                                    </div>
-                                </div>
-                            @endif
-                        @endforeach
+                        <div class="reviewer  d-flex  justify-content-left mt-3">
+                            <div class="img">
+                                <img src="{{ asset('img/review/reviewer.png') }}" alt="プロフィール写真">
+                            </div>
+                            <div class="comment text-md-start">
+                                <p class="name mb-1">{{ $good_user->nickname }}</p>
+                                <p class="review text-wrap">{{ $good_user->evaluation_comment }}</p>
+                                <p>{{ $good_user->call_end_time }}</p>
+                            </div>
+                        </div>
                     @endforeach
                 </div>
 
                 <div class="tab-pane fade " id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
                     @foreach ($bad_users as $bad_user)
-                        @foreach ($bad_reviews as $bad_review)
-                            @if ($loop->parent->first)
-                                <div class="reviewer d-flex justify-content-left mt-3">
-                                    <div class="img">
-                                        <img src="{{ asset('img/review/reviewer.png') }}" alt="プロフィール写真">
-                                    </div>
-                                    <div class="comment div flex-column text-left ">
-                                        <p class="name mb-1">{{ $bad_user->nickname }}</p>
-                                        <p class="review text-wrap">{{ $bad_review->evaluation_comment }}</p>
-                                        <p>{{ $bad_review->call_end_time }}</p>
-                                    </div>
-                                </div>
-                            @endif
-                        @endforeach
+                        <div class="reviewer d-flex justify-content-left mt-3">
+                            <div class="img">
+                                <img src="{{ asset('img/review/reviewer.png') }}" alt="プロフィール写真">
+                            </div>
+                            <div class="comment div flex-column text-left ">
+                                <p class="name mb-1">{{ $bad_user->nickname }}</p>
+                                <p class="review text-wrap">{{ $bad_user->evaluation_comment }}</p>
+                                <p>{{ $bad_user->call_end_time }}</p>
+                            </div>
+                        </div>
                     @endforeach
                 </div>
             </div>
