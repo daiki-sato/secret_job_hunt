4f84c65a0fbd4457cbf6e14e45e652ce52a882de
【daiki】 <daiki20010705@icloud.com>
2022-05-12 22:21:59 +0900

    sample削除

--- src/app/Http/Controllers/Api/InterviewController.php
+++ src/app/Http/Controllers/Api/InterviewController.php
@@ -30,28 +30,4 @@ class InterviewController extends Controller
 
         return "OK";
     }
-
-    public function sample(Request $request)
-    {
-        $solversId = [2, 4, 5];
-        foreach ($solversId as $solver_id) {
-            $interviews = Interview::create(
-                [
-                    'user_id' => 1,
-                    'solver_id' => $solver_id,
-                ]
-            );
-            $interview_id = $interviews->id;
-            $interview_time = InterviewTime::create(
-                [
-                    'interview_id' => $interview_id,
-                    'from_what_time' => "2020/11/11 12:10",
-                    'to_what_time' => "2020/11/11 12:20",
-                ]
-            );
-        }
-
-        // return "OK";
-        return redirect('my-page');
-    }
 }
