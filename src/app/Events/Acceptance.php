745d97dc1896c7e70f6f041579eab3a3bce0c734
【daiki】 <daiki20010705@icloud.com>
2022-05-13 17:51:56 +0900

    無駄な空白削除

--- src/app/Events/Acceptance.php
+++ src/app/Events/Acceptance.php
@@ -8,13 +8,10 @@ use App\Models\InterviewTime;
 use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-
 
 class Acceptance
 {
