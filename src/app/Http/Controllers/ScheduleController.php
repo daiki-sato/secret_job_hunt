d2b63c75e3f50e521a5a8707f3dc994112c720b6
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-20 01:40:35 +0900

    confirmed_interview_dateにnullがはいっている場合は処理を行わないようにしたい。

--- src/app/Http/Controllers/ScheduleController.php
+++ src/app/Http/Controllers/ScheduleController.php
@@ -17,18 +17,29 @@ class ScheduleController extends Controller
     public function solver()
     {
         $id = Auth::id();
-        $confirmed_interviews = Call::where('solver_id', $id)
-            ->join('users', 'users.id', '=', 'calls.user_id')
-            ->select('users.nickname', 'calls.confirmed_interview_date')
-            ->get();
+        // dd($id);
+        $confirmed_interview_date = Call::where('solver_id', $id)
+            ->value('confirmed_interview_date');
+        // dd($confirmed_interview_date);
+        if ($confirmed_interview_date != null) {
 
-        $unanswered_interviews = InterviewTime::where('is_agreement', null)
-            ->join('interviews', 'interviews.id', '=', 'interview_times.interview_id')
-            ->join('users', 'users.id', '=', 'interviews.user_id')
-            ->where('interviews.solver_id', $id)
-            ->select('users.nickname', 'interview_times.from_what_time')
-            ->get();
+            $id = Auth::id();
 
-        return view('schedule_solver.index', ['confirmed_interviews' => $confirmed_interviews, 'unanswered_interviews' => $unanswered_interviews]);
+            $confirmed_interviews = Call::where('solver_id', $id)
+                ->join('users', 'users.id', '=', 'calls.user_id')
+                ->select('users.nickname', 'calls.confirmed_interview_date')
+                ->get();
+
+            $unanswered_interviews = InterviewTime::where('is_agreement', null)
+                ->join('interviews', 'interviews.id', '=', 'interview_times.interview_id')
+                ->join('users', 'users.id', '=', 'interviews.user_id')
+                ->where('interviews.solver_id', $id)
+                ->select('users.nickname', 'interview_times.from_what_time')
+                ->get();
+
+            return view('schedule_solver.index', ['confirmed_interviews' => $confirmed_interviews, 'unanswered_interviews' => $unanswered_interviews]);
+        } else {
+            return false;
+        }
     }
 }
