a9dd326c7043d2a3abdbfc46a60e99ed627f79da
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-19 21:08:45 +0900

    管理画面の換金状況、お問い合わせ状況の反映

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Models\Wallet;
+use App\Models\User;
+use Carbon\Carbon;
+use PhpParser\Node\Expr\AssignOp\Concat;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $contacts = Contact::all();
+        $wallets = Wallet::all();
+        $users = User::with(['contacts','wallet'])->get();
+        
+        return view('admin.index', compact('contacts','wallets','users'));
     }
-
 }
--- src/database/migrations/2022_04_18_145222_create_wallets_table.php
+++ src/database/migrations/2022_04_18_145222_create_wallets_table.php
@@ -18,7 +18,7 @@ class CreateWalletsTable extends Migration
             $table->unsignedInteger('user_id');
             $table->integer('balance');
 
-            $table->timestamps();
+            $table->timestamps('timestamp');
             $table->softDeletes();
 
             // $table->foreign('user_id')->references('id')->on('users');
