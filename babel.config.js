c5aa23769499ce193b9428034b2045e80e8bea11
【daiki】 <daiki20010705@icloud.com>
2022-05-22 01:12:00 +0900

    通話機能実装

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+// babel.config.js
+module.exports =  {
+  "presets": ["module:metro-react-native-babel-preset"],
+  "env": {
+    "production": {
+      "plugins": [
+        "ignite-ignore-reactotron",
+        "@babel/plugin-proposal-class-properties"
+      ]
+    }
+  }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('/interviewsData', 'Api\InterviewController@post');
 Route::get('/userBalance/{userId}', 'Api\GetUserBalanceController@index');
+Route::get('/callRoomId/{threadId}', 'Api\GetCallController@index');
