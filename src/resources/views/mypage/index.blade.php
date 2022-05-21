9054b51df30f0516f06e498d18115e1169f61ab9
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-21 19:54:48 +0900

    mypageレイアウト変えた

--- src/resources/views/mypage/index.blade.php
+++ src/resources/views/mypage/index.blade.php
@@ -7,94 +7,192 @@
                 <h2 class="mypage-title">マイページ</h2>
                 <p class="mr-5 contact-title">お問い合せ</p>
             </div>
-            <div class="text-center profile_top">
-                <img src="" alt="profile_img" class="m-3 profile_img">
-                <p class="m-2 h3">{{ $user->nickname }}</p>
-            </div>
-            <div class="pl-4 mx-5 text-left ticket_container">
-                <p class="py-3 h3 info_title">所持チケット</p>
-                <div class="pt-2 pb-4 ticket_detail">
-                    <p class="pl-5 m-0 h4 ticket_number">{{ $balance / 1200 }}枚</p>
-                    <button class="px-5 mx-4 py-2 exchange-button btn btn-danger" type="button" data-toggle="modal"
-                        data-target="#exampleModalCenter">
-                        チケットの購入
-                    </button>
+            <div class="my-5 d-flex flex-row mypage-wrapper">
+                <div class="m-5 text-center profile_top">
+                    <img src="" alt="profile_img" class="m-3 profile_img">
+                    <p class="m-2 h3">{{ $user->nickname }}</p>
                 </div>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-                    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalCenterTitle">購入枚数の選択</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                <div class="m-5 mypage-inner">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="wallet-tab" data-toggle="tab" href="#wallet" role="tab" aria-controls="wallet" aria-selected="true">獲得金額</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">登録情報</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="wallet" role="tabpanel" aria-labelledby="wallet-tab">
+                        <div class="p-5 ticket_detail">
+                            <p class="pl-5 m-0 h4 ticket_number">{{ $balance / 1200 }}枚</p>
+                            <button class="px-5 mx-4 py-2 exchange-button btn btn-danger" type="button" data-toggle="modal"
+                                data-target="#exampleModalCenter">
+                                チケットの購入
+                            </button>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalCenterTitle">購入枚数の選択</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="{{ route('paypay') }}" method="GET" class="flex">
+                                            <img class="img-fluid h-50 w-50 " src="{{ asset('img/paypay_1.jpg') }}" alt="">
+                                            <div class="ticketbypaypay pt-4 pl-4">
+                                                <p>1枚1200円</p>
+                                                枚数の選択：
+                                                <select name="ticket" id="">
+                                                    <option value="1" name="ticket">1</option>
+                                                    <option value="2" name="ticket">2</option>
+                                                    <option value="3" name="ticket">3</option>
+                                                    <option value="4" name="ticket">4</option>
+                                                </select>
+                                            </div>
+                                            <div class="mt-4">
+                                                <input type="submit" class="w-100 btn btn-primary" value="PayPayで購入手続きへ">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                <form action="{{ route('paypay') }}" method="GET" class="flex">
-                                    <img class="img-fluid h-50 w-50 " src="{{ asset('img/paypay_1.jpg') }}" alt="">
-                                    <div class="ticketbypaypay pt-4 pl-4">
-                                        <p>1枚1200円</p>
-                                        枚数の選択：
-                                        <select name="ticket" id="">
-                                            <option value="1" name="ticket">1</option>
-                                            <option value="2" name="ticket">2</option>
-                                            <option value="3" name="ticket">3</option>
-                                            <option value="4" name="ticket">4</option>
-                                        </select>
-                                    </div>
-                                    <div class="mt-4">
-                                        <input type="submit" class="w-100 btn btn-primary" value="PayPayで購入手続きへ">
-                                    </div>
-                                </form>
+                        </div>                        
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <div class="p-5 text-center profile_detail">
+                            <table class="mb-5 profile_table">
+                                <tbody>
+                                    <tr>
+                                        <td class="py-3">名前</td>
+                                        <td class="pl-4 py-3">{{ $user->first_name }} {{ $user->last_name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="py-3">カナ</td>
+                                        <td class="pl-4 py-3">{{ $user->first_name_ruby }} {{ $user->last_name_ruby }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="py-3">ニックネーム</td>
+                                        <td class="pl-4 py-3">{{ $user->nickname }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="py-3">メールアドレス</td>
+                                        <td class="pl-4 py-3">{{ $user->email }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="py-3">性別</td>
+                                        <td class="pl-4 py-3">{{ $user->sex }}</td>
+                                    </tr>
+                                    @if ($user->role_id == 3)
+                                        <tr>
+                                            <td class="py-3">勤務先</td>
+                                            <td class="pl-4 py-3">{{ $user->company }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="py-3">部署</td>
+                                            <td class="pl-4 py-3">{{ $user->department }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="py-3">勤務年数</td>
+                                            <td class="pl-4 py-3">{{ $user->working_period }}年</td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                            <a href="{{ route('user_edit', ['id' => $user->id]) }}" class="px-5 py-2 edit-button">情報を更新する</a>
+                        </div>
+                    </div>
+                    </div>
+                    {{-- <div class="pl-4 mx-5 text-left ticket_container">
+                    <p class="py-3 h3 info_title">所持チケット</p>
+                    <div class="pt-2 pb-4 ticket_detail">
+                        <p class="pl-5 m-0 h4 ticket_number">{{ $balance / 1200 }}枚</p>
+                        <button class="px-5 mx-4 py-2 exchange-button btn btn-danger" type="button" data-toggle="modal"
+                            data-target="#exampleModalCenter">
+                            チケットの購入
+                        </button>
+                    </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalCenterTitle">購入枚数の選択</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('paypay') }}" method="GET" class="flex">
+                                        <img class="img-fluid h-50 w-50 " src="{{ asset('img/paypay_1.jpg') }}" alt="">
+                                        <div class="ticketbypaypay pt-4 pl-4">
+                                            <p>1枚1200円</p>
+                                            枚数の選択：
+                                            <select name="ticket" id="">
+                                                <option value="1" name="ticket">1</option>
+                                                <option value="2" name="ticket">2</option>
+                                                <option value="3" name="ticket">3</option>
+                                                <option value="4" name="ticket">4</option>
+                                            </select>
+                                        </div>
+                                        <div class="mt-4">
+                                            <input type="submit" class="w-100 btn btn-primary" value="PayPayで購入手続きへ">
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="pl-4 mx-5 text-left profile_container">
-                <p class="py-3 h3 info_title">登録者情報</p>
-                <div class="pl-4 py-4 text-center profile_detail">
-                    <table class="mb-5 profile_table">
-                        <tbody>
-                            <tr>
-                                <td>名前</td>
-                                <td>{{ $user->first_name }} {{ $user->last_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>カナ</td>
-                                <td>{{ $user->first_name_ruby }} {{ $user->last_name_ruby }}</td>
-                            </tr>
-                            <tr>
-                                <td>ニックネーム</td>
-                                <td>{{ $user->nickname }}</td>
-                            </tr>
-                            <tr>
-                                <td>メールアドレス</td>
-                                <td>{{ $user->email }}</td>
-                            </tr>
-                            <tr>
-                                <td>性別</td>
-                                <td>{{ $user->sex }}</td>
-                            </tr>
-                            @if ($user->role_id == 3)
+                    </div>
+                    <div class="pl-4 mx-5 text-left profile_container">
+                    <p class="py-3 h3 info_title">登録者情報</p>
+                    <div class="pl-4 py-4 text-center profile_detail">
+                        <table class="mb-5 profile_table">
+                            <tbody>
                                 <tr>
-                                    <td>勤務先</td>
-                                    <td>{{ $user->company }}</td>
+                                    <td>名前</td>
+                                    <td>{{ $user->first_name }} {{ $user->last_name }}</td>
                                 </tr>
                                 <tr>
-                                    <td>部署</td>
-                                    <td>{{ $user->department }}</td>
+                                    <td>カナ</td>
+                                    <td>{{ $user->first_name_ruby }} {{ $user->last_name_ruby }}</td>
                                 </tr>
                                 <tr>
-                                    <td>勤務年数</td>
-                                    <td>{{ $user->working_period }}年</td>
+                                    <td>ニックネーム</td>
+                                    <td>{{ $user->nickname }}</td>
                                 </tr>
-                            @endif
-                        </tbody>
-                    </table>
-                    <a href="{{ route('user_edit', ['id' => $user->id]) }}" class="px-5 py-2 edit-button">情報を更新する</a>
+                                <tr>
+                                    <td>メールアドレス</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
+                                <tr>
+                                    <td>性別</td>
+                                    <td>{{ $user->sex }}</td>
+                                </tr>
+                                @if ($user->role_id == 3)
+                                    <tr>
+                                        <td>勤務先</td>
+                                        <td>{{ $user->company }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>部署</td>
+                                        <td>{{ $user->department }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>勤務年数</td>
+                                        <td>{{ $user->working_period }}年</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                        <a href="{{ route('user_edit', ['id' => $user->id]) }}" class="px-5 py-2 edit-button">情報を更新する</a>
+                    </div>
+                    </div> --}}
                 </div>
             </div>
             <p class="px-5 py-2 account-button"><a href="" class="text-danger">アカウント削除</a></p>
