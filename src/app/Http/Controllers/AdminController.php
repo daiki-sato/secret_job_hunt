1a45895777dbc922c96923974be48027cd0e6512
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-22 13:34:15 +0900

    不要なコード削除

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -12,8 +12,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $contacts = Contact::all();
-        $wallets = Wallet::all();
+        
         $users = User::with(['contacts','wallet'])->get();
         
         return view('admin.index', compact('contacts','wallets','users'));
