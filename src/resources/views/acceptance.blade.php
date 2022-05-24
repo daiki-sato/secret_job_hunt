86d5ce338ba34b12059a6e6367566234c2c572fd
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-05-24 16:21:00 +0900

    承諾メール正式

--- src/resources/views/acceptance.blade.php
+++ src/resources/views/acceptance.blade.php
@@ -1,4 +1,12 @@
-面談が受理されました、チャットを始めましょう。
+面談の申し込み依頼が承諾されましたためお知らせいたします。
 
-面談者：{{ $solver_name }}
-日時：{{ $from_what_time }}〜{{ $to_what_time }}
+■該当URL
+<a href="http://localhost/"></a>
+
+■日時
+{{ $from_what_time }}〜{{ $to_what_time }}
+
+■面談者
+{{ $solver_name }}
+
+Anovey
