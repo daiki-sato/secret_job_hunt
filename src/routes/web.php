c724bf2deab048221f18873de05fbb6940e5287d
【daiki】 <daiki20010705@icloud.com>
2022-05-29 23:53:42 +0900

    チャットの見た目キレイにした

--- src/routes/web.php
+++ src/routes/web.php
@@ -73,6 +73,13 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/add', 'EvaluationController@add')->name('evaluation_add');
     });
 
+    // メッセージ
+    Route::prefix('message')->group(function () {
+        Route::get('/get/{threadId}', 'Ajax\MessageController@get')->name('message.get');
+        Route::post('/post', 'Ajax\MessageController@create'); // チャット登録
+        Route::post('/delete/{messageId}', 'Ajax\MessageController@delete');
+    });
+
     //支払い(paypay)
     Route::prefix('paypay')->group(function () {
         //ルーティングで支払いができないように適当な文字列を追加
