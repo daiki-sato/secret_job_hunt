27551636d097905512efac4b3ce2ca7cd9055af7
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-29 14:02:03 +0900

    パスワードリセット周りを日本語に直した

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="pt-5 w-50 mx-auto auth_login">
-        <h2 class="auth_login_title text-center">{{ __('Reset Password') }}</h2>
+        <h2 class="auth_login_title text-center">パスワードの再設定</h2>
         @if (session('status'))
         <div class="alert alert-success" role="alert">
             {{ session('status') }}
@@ -11,7 +11,10 @@
         <form action="{{ route('password.email') }}" method="POST" class="mt-4 mb-2 form-login">
             @csrf
             <div class="pt-2 pb-4 mb-2 text-left">
-                <label for="inputEmail" class="input-area">{{ __('E-Mail Address') }}</label>
+                <div class="pb-2">
+                    <label for="inputEmail" class="h5 input-area">メールアドレス</label>
+                    <span class="pl-3">ご登録いただいたメールアドレスをご入力ください</span>
+                </div>
                 <input id="email" placeholder="メールアドレス" type="email"
                 class="form-control @error('email') is-invalid @enderror"
                 name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -23,7 +26,7 @@
                 @enderror
             </div>
             <div class="mt-4 mb-2 px-2 text-center login-button">
-                <button type="submit" class="fw-bold text-light btn btn-lg btn-green btn-block">{{ __('Send Password Reset Link') }}</button>
+                <button type="submit" class="fw-bold text-light btn btn-lg btn-green btn-block">再設定用のメールを送信する</button>
             </div>        
         </form>
     </div>
