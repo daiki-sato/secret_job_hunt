eac79fac3692b8fbafd2b7fe13d3b52e2538f4c1
hinatakahashi <hina0414takahashi@gmail.com>
2022-05-29 23:24:18 +0900

    細々修正

--- src/app/Http/Controllers/EvaluationController.php
+++ src/app/Http/Controllers/EvaluationController.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Console\Presets\React;
 use Illuminate\Http\Request;
 use App\Models\Call;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class EvaluationController extends Controller
@@ -17,10 +19,12 @@ class EvaluationController extends Controller
 
     public function add(Request $request)
     {
+        $id = Auth::id();
+        $user = User::find($id);
         Call::create(
             [
                 'thread_id' => 1,
-                'user_id' => 1,
+                'user_id' => $user->id,
                 'solver_id' => 2,
                 'evaluation' => $request->evaluation,
                 'evaluation_comment' => $request->evaluation_comment,
